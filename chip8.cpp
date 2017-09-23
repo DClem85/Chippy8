4d1a38c852303838280820917e71b630ed7f944a
D.C <danny.l.clemens@gmail.com>
2017-09-23 07:42:43 -0400

    starting opcode programming

--- chip8.cpp
+++ chip8.cpp
@@ -27,10 +27,96 @@ void chip8::emulateCycle()
 		}
 		break;
 
+	case 0x1000: //jump to address
+		pc = opcode & 0x0FFF;
+	break;
+
+	case 0x2000:
+		stack[sp] = pc;
+		++sp;
+		pc = opcode & 0x0FFF;
+	break;
+	
+	case 0x3000:
+		if ([V(opcode & 0x0F00) >> 8] == (opcode & 0x00FF))
+			pc += 4; //skip the next instruction
+		else
+			pc += 2; 
+	break;
+
+	case 0x4000:
+		if ([V(opcode & 0x0F00) >> 8] != (opcode & 0x00FF))
+			pc += 4; //skip the next instruction
+		else
+			pc += 2;
+	break;
+
+	case 0x5000: // 5XY0 skip if VX == VY
+		if ([V(opcode & 0x0F00) >> 8] == (opcode & 0x00F0 >> 4))
+			pc += 4; //skip the next instruction
+		else
+			pc += 2;
+	break;
+
+	case 0x6000: //6XNN sets VX to NN
+		V[(opcode & 0x0F00) >> 8] = opcode & 0x00FF;
+		pc += 2;
+		break;
+
+	case 0x7000: //7XNN Vx == NN
+		V[(opcode & 0x0F00) >> 8] += opcode & 0x00FF;
+		pc += 2;
+		break;
+	
+	case 0x8000: //oh no! Multiples
+		switch (opcode & 0x000F)
+		{
+			case 0x0000 // 08XY0 VX=Y
+				V[opcode & 0x0F00 >> 8] = (opcode & 0x00F0 >> 4)
+				pc += 2;
+				break;
+
+			case 0x0001 //08XY1 VX = VX|VY
+				V[opcode & 0x0F00] |= (opcode & 0x00F0 >> 4))
+				pc += 2;
+				break;
+
+			case 0x0002 //08XY2 VX = VX&VY
+				V[opcode & 0x0F00] &= (opcode & 0x00F0 >> 4))
+				pc += 2;
+				break;
+
+			case 0x0003 //08XY3 VX = VX ^ VY
+				V[opcode & 0x0F00] ^= (opcode & 0x00F0 >> 4))
+				pc += 2;
+				break;
+
+				case 0x0004 //08XY4 VX = VX + VY
+					if V[(opcode & 0x00F0) >> 4] > (0xFF - V[(opcode & 0x0F00) >> 8])
+						V[0xF] = 1; //there is a carry
+					else
+						V[0xF] = 0; //there is no carry
+				V[opcode & 0x0F00] += (opcode & 0x00F0 >> 4))
+				pc += 2;
+				break;
+
+			case 0x0005 //08XY5 VX = VX - VY
+				V[opcode & 0x0F00] -= (opcode & 0x00F0 >> 4))
+				pc += 2;
+				break;
+
+			case 0x0006 //08XY6 VX = VX - VY
+				V[opcode & 0x0F00] = (opcode & 0x00F0 << 1))
+				pc += 2;
+				break;
+		}
+
+
 	case 0xA000: // 
 		I = opcode & 0x0FFF;
 		pc += 2;
 		break;
+
 
 	
 
